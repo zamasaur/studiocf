2c0dfcf5d7ab344c196b81a3c0c330ad629ea0e0
zamasaur <zamasaur@hotmail.com>
2025-07-18 09:04:36 +0200

    clean

--- js/main.js
+++ js/main.js
@@ -9,20 +9,6 @@ window.addEventListener("load", () => {
             return response.json();
         })
         .then(jsondata => {
-            /*const text = jsondata["Diritto del mercato finanziario e degli intermediari"][0].question;
-
-            document.querySelector('textarea').value = text;
-
-            var utterance = new SpeechSynthesisUtterance();
-            utterance.text = text;
-            utterance.lang = 'it-IT';
-
-            //utterance.pitch = 0.001;
-            utterance.rate = 0.75;
-            //utterance.voice = voices[0];
-            //utterance.volume = 2;
-            window.speechSynthesis.speak(utterance);*/
-            
             let model = new Model(jsondata);
             let view = new View();
             let controller = new Controller(model, view);
